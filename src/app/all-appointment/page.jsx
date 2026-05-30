93b860326979bf945ffd6e778ddc09f7a9fe450f
rsdrahi <rsdrahi13@gmail.com>
2026-05-30 18:22:39 +0600

    app-appointment page finish with better UI design.

--- src/app/all-appointment/page.jsx
+++ src/app/all-appointment/page.jsx
@@ -1,8 +1,15 @@
+import AppointmentCard from '@/components/AppointmentCard';
 import { SearchItems } from '@/components/SearchItems';
 import { SearchField } from '@heroui/react';
 import React from 'react';
 
-const AllAppointmentPage = () => {
+const AllAppointmentPage = async () => {
+
+  const res = await fetch(`${process.env.NEXT_CLIENT_API_URL}/all-appointment`)
+  const appointments = await res.json();
+  console.log(appointments, "Data");
+
+
   return (
     <div>
       <div className='space-y-3'>
@@ -11,6 +18,14 @@ const AllAppointmentPage = () => {
         <div className='flex justify-center'>
           <SearchItems></SearchItems>
         </div>
+      </div>
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 my-6'>
+        {
+          appointments.map(appointment => <AppointmentCard
+            key={appointment._id}
+            appointment={appointment}
+          ></AppointmentCard>)
+        }
       </div>
     </div>
   );
---
+++ src/components/AppointmentCard.jsx
@@ -0,0 +1,40 @@
+import { Button, Card, Separator } from '@heroui/react';
+import Image from 'next/image';
+import React from 'react';
+
+const AppointmentCard = ({ appointment }) => {
+
+  const { name, image, description, fee, specialty, hospital } = appointment
+
+  return (
+    <Card>
+      <div className='relative aspect-square'>
+        <Image
+          src={image}
+          alt='Doctor Image'
+          // height={200}
+          // width={200}
+          fill
+          className='w-full object-cover rounded-lg'
+        />
+      </div>
+      <div>
+        <h3 className='font-bold text-2xl'>{name}</h3>
+        <h2 className='font-medium'>{specialty}</h2>
+      </div>
+      <div>
+        <p className='font-medium text-muted'>{description}</p>
+      </div>
+      <Separator></Separator>
+      <div className='flex justify-between items-center'>
+        <div>
+          <p> <span className='font-bold'>Fee:</span> ৳{fee}</p>
+          <p> <span className='font-bold'>Visit:</span> {hospital}</p>
+        </div>
+        <Button>View Details</Button>
+      </div>
+    </Card>
+  );
+};
+
+export default AppointmentCard;
