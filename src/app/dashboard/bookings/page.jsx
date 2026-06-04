e776c763dd21dc1cf0dfa134e6e239ab5fe45939
rsdrahi <rsdrahi13@gmail.com>
2026-06-04 15:16:11 +0600

    On the booking page show data. and created bookingCard.

--- src/app/dashboard/bookings/page.jsx
+++ src/app/dashboard/bookings/page.jsx
@@ -1,9 +1,27 @@
+import BookingCard from '@/components/dashboard/BookingCard';
 import React from 'react';
 
-const BookingsPage = () => {
+const BookingsPage = async () => {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book-appointment`, {
+    method: "GET",
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+  const bookings = await res.json();
+  console.log(bookings, "Data");
+
   return (
     <div>
-      Bookings
+      <h3 className='font-bold text-2xl text-center my-4'>Bookings</h3>
+      <div>
+        {
+          bookings.map(booking => <BookingCard key={booking._id}
+          booking={booking}
+          ></BookingCard>)
+        }
+      </div>
     </div>
   );
 };
---
+++ src/components/dashboard/BookingCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookingCard = ({ booking }) => {
+  console.log(booking, "Booking");
+  const { doctorName, patientName, date, time } = booking
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default BookingCard;
